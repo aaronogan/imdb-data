ffe5a196ed44a9716c63eab412c54f1ae400dbe3
Aaron Ogan <aaron.ogan@gmail.com>
2021-03-20 16:38:25 -0400

    Basic summary working

--- wikipedia-data.py
+++ wikipedia-data.py
@@ -2,13 +2,35 @@
 import argparse
 import wikipedia
 
-def main():
+def __get_parser():
     parser = argparse.ArgumentParser(description='Wikipedia Data')
-    parser.add_argument('-s', '--search', help='Search', required=True)
+    parser.add_argument('-s', '--search', help='Search term', required=True)
+    parser.add_argument('-c', '--count', help='Result count', type=int, default=5, required=False)
 
-    args = vars(parser.parse_args())
-    search = args['search']
-    print(wikipedia.search(search))
+    return parser
+
+def __get_arguments():
+    parser = __get_parser()
+    return vars(parser.parse_args())
+
+def __get_search_results(arguments):
+    search = arguments['search']
+    count = arguments['count']
+    return wikipedia.search(search, results=count, suggestion=True)
+
+def __get_pages(arguments):
+    search_results = __get_search_results(arguments)
+    titles = search_results[0]
+    return [wikipedia.page(title=title, auto_suggest=False, redirect=True, preload=True) for title in titles]
+
+def main():
+    args = __get_arguments()
+    pages = __get_pages(args)
+
+    for page in pages:
+        print('==============')
+        print(page.links)
+        print(page.summary)
 
 if __name__ == "__main__": main()
 
